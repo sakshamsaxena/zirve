a59afe47356db8530a3ccc968ea837d9008eafab
sakshamsaxena <ontenth@live.com>
2017-01-04 21:03:50 +0530

    Added scrollify and pagination

--- js/app.js
+++ js/app.js
@@ -24,3 +24,32 @@
 	document.body.addEventListener('dragover', drag_over, false);
 	document.body.addEventListener('drop', drop, false);
 })();
+
+$(function () {
+	$.scrollify({
+		section: ".diapo",
+		scrollbars: false,
+		before: function (i, panels) {
+			var ref = panels[i].attr("data-section-name");
+			$(".pagination .active").removeClass("active");
+			$(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+		},
+		afterRender: function () {
+			var pagination = "<ul class=\"pagination\">";
+			var activeClass = "";
+			$(".diapo").each(function (i) {
+				activeClass = "";
+				if (i === 0) {
+					activeClass = "active";
+				}
+				pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+			});
+
+			pagination += "</ul>";
+
+			$("body").append(pagination);
+		}
+	});
+	
+	$(".pagination a").on("click", $.scrollify.move);
+});
