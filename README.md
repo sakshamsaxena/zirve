cae6af13b9b4e9bd3c0dd4444f5ed4bb31f707a2
sakshamsaxena <ontenth@live.com>
2017-01-05 20:50:09 +0530

    Did too much without commits. Added menu button and updated fonts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ## Zirve
+
+Simple Fron End for My Blog.
+
+### Installation
+
+1. Clone this repository using `git clone https://github.com/sakshamsaxena/zirve.git`. You must have Git installed in your system.
+2. Change directory to this folder using `cd zirve`.
+3. Install dependecies using `npm install`. You must have Node.js installed in your system to run this.
+
+That's it! The `index.html` has everything. Hack away!
--- js/app.js
+++ js/app.js
@@ -1,29 +1,39 @@
 /* Application Sripts */
+function drag_start(event) {
+	event.originalEvent.dataTransfer.setData("text/plain", (event.pageX) + ',' + (event.pageY));
+}
 
-(function () {
-	function drag_start(event) {
-		var style = window.getComputedStyle(event.target, null);
-		event.dataTransfer.setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY));
-	}
+function drag_over(event) {
+	event.preventDefault();
+	return false;
+}
 
-	function drag_over(event) {
-		event.preventDefault();
-		return false;
-	}
+function drop(event) {
+	var d = event.originalEvent.dataTransfer.getData("text/plain").split(',');
+	var oldX = parseInt(d[0]);
+	var oldY = parseInt(d[1]);
+	
+	var dm = $("#dragme");
+	var offsetX = dm.offset().left;
+	var offsetY = dm.offset().top;
+	
+	var top = parseFloat(event.pageY - oldY + offsetY);
+	var left = parseFloat(event.pageX - oldX + offsetX);
+	
+	dm.offset({
+		top: top,
+		left: left
+	});
+	
+	event.preventDefault();
+	return false;
+}
 
-	function drop(event) {
-		var offset = event.dataTransfer.getData("text/plain").split(',');
-		var dm = document.getElementById('dragme');
-		dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
-		dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
-		event.preventDefault();
-		return false;
-	}
-	var dm = document.getElementById('dragme');
-	dm.addEventListener('dragstart', drag_start, false);
-	document.body.addEventListener('dragover', drag_over, false);
-	document.body.addEventListener('drop', drop, false);
-})();
+$("#dragme").on("dragstart", drag_start);
+
+$("body").on("dragover", drag_over);
+
+$("body").on("drop", drop);
 
 $(function () {
 	$.scrollify({
@@ -50,6 +60,6 @@ $(function () {
 			$("body").append(pagination);
 		}
 	});
-	
+
 	$(".pagination a").on("click", $.scrollify.move);
 });
