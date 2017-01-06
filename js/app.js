da356b8801e6d21c760727440f8fa878dbc2d6c0
sakshamsaxena <ontenth@live.com>
2017-01-06 17:21:30 +0530

    Added some old school effects

--- js/app.js
+++ js/app.js
@@ -12,21 +12,48 @@ function drop(event) {
 	var d = event.originalEvent.dataTransfer.getData("text/plain").split(',');
 	var oldX = parseInt(d[0]);
 	var oldY = parseInt(d[1]);
-	
+
 	var dm = $("#dragme");
 	var offsetX = dm.offset().left;
 	var offsetY = dm.offset().top;
-	
+
 	var top = parseFloat(event.pageY - oldY + offsetY);
 	var left = parseFloat(event.pageX - oldX + offsetX);
-	
+
 	dm.offset({
 		top: top,
 		left: left
 	});
-	
+
 	event.preventDefault();
 	return false;
+}
+
+function anim(i, panels) {
+	var q = {
+		opacity: 1
+	};
+	var r = {
+		duration: 1000,
+		queue: true
+	};
+	
+	$(panels[i]).find(".t1")
+		.animate(q, r)
+		.queue(function () {
+			$(panels[i]).find(".t2").animate(q, r);
+			$(this).dequeue();
+		})
+		.delay(3000)
+		.queue(function () {
+			$(panels[i]).find(".t3").animate(q, r);
+			$(this).dequeue();
+		})
+		.delay(1500)
+		.queue(function () {
+			$(panels[i]).find(".t4").animate(q, r);
+			$(this).dequeue();
+		});
 }
 
 $("#dragme").on("dragstart", drag_start);
@@ -35,15 +62,16 @@ $("body").on("dragover", drag_over);
 
 $("body").on("drop", drop);
 
+$(document).ready(function() {
+	setTimeout(function() {
+		anim(0, document.getElementsByClassName("diapo"));
+	}, 200)
+})
+
 $(function () {
 	$.scrollify({
 		section: ".diapo",
 		scrollbars: false,
-		before: function (i, panels) {
-			var ref = panels[i].attr("data-section-name");
-			$(".pagination .active").removeClass("active");
-			$(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-		},
 		afterRender: function () {
 			var pagination = "<ul class=\"pagination\">";
 			var activeClass = "";
@@ -58,6 +86,15 @@ $(function () {
 			pagination += "</ul>";
 
 			$("body").append(pagination);
+		},
+		before: function (i, panels) {
+			var ref = panels[i].attr("data-section-name");
+			$(".pagination .active").removeClass("active");
+			$(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+		},
+		after: function (i, panels) {
+			anim(i, panels)
+			console.log($(panels[i]).find(".t1"))
 		}
 	});
 
