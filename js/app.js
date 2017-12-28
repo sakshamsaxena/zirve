6ca86a45c88ee41cae2678bb866a9b774301ffaa
Saksham Saxena <saksham_saxena@outlook.com>
2017-12-28 23:02:45 +0530

    Added some responsive rules

--- js/app.js
+++ js/app.js
@@ -1,10 +1,86 @@
 /* Application Sripts */
+
+$(document).ready(function() {
+    $(".home .t1, .home .t2, .home .t3, .home .t4").css("opacity", "0");
+    $(".about .t3, .about .t4, .proj .t3, .proj .t4").hide();
+    $(".bio, .projects").toggleClass("darken-2");
+});
+
 $(window).on("load", function() {
     setTimeout(function() {
         anim(0, document.getElementsByClassName("diapo"))
     }, 200);
 });
 
+$(function() {
+    $.scrollify({
+        section: ".diapo",
+        scrollbars: true,
+        overflowScroll: true,
+        touchScroll: false,
+        after: function(i, panels) {
+            anim(i, panels)
+        }
+    });
+    if (window.innerWidth < 768) {
+        $.scrollify.disable();
+    }
+});
+
+// Handlers for Home Page
+$(".aboutBtn, .contBtn, .projBtn").click(function(e) {
+    //TODO : Write custom jQ style smooth scroll
+    if(window.innerWidth > 768) {
+        $.scrollify.move(e.target.getAttribute("href"));
+    }
+    return true
+});
+
+// TODO : Update height for all diapo on click
+// Handlers for About Page
+$(".bio").click(function() {
+    $(".about .t2").show();
+    $(".about .t3, .about .t4").hide();
+    $(this).removeClass("darken-2");
+    $(".interests, .ideas").addClass("darken-2");
+});
+$(".interests").click(function() {
+    $(".about .t3").show();
+    $(".about .t2, .about .t4").hide();
+    $(this).removeClass("darken-2");
+    $(".bio, .ideas").addClass("darken-2");
+});
+$(".ideas").click(function() {
+    $(".about .t4").show();
+    $(".about .t3, .about .t2").hide();
+    $(this).removeClass("darken-2");
+    $(".bio, .interests").addClass("darken-2");
+});
+
+// Handlers for Work Exp Page
+$(".projects").click(function() {
+    $(".proj .t2").show();
+    $(".proj .t3, .proj .t4").hide();
+    $(this).removeClass("darken-2");
+    $(".respon, .workexp").addClass("darken-2");
+    $.scrollify.update();
+});
+$(".respon").click(function() {
+    $(".proj .t3").show();
+    $(".proj .t2, .proj .t4").hide();
+    $(this).removeClass("darken-2");
+    $(".projects, .ideas").addClass("darken-2");
+    $.scrollify.update();
+});
+$(".workexp").click(function() {
+    $(".proj .t4").show();
+    $(".proj .t3, .proj .t2").hide();
+    $(this).removeClass("darken-2");
+    $(".projects, .respon").addClass("darken-2");
+    $.scrollify.update();
+});
+
+// Helper Functions
 function anim(i, panels) {
     var q = {
         opacity: 1
@@ -31,68 +107,3 @@ function anim(i, panels) {
             $(this).dequeue();
         });
 }
-
-$(".aboutBtn, .contBtn, .projBtn").click(function(e) {
-    $.scrollify.move(e.target.getAttribute("href"));
-    return true
-});
-
-$(".bio").click(function() {
-    $(".about .t2").show();
-    $(".about .t3, .about .t4").hide();
-    $(this).removeClass("darken-2");
-    $(".interests, .ideas").addClass("darken-2");
-});
-$(".interests").click(function() {
-    $(".about .t3").show();
-    $(".about .t2, .about .t4").hide();
-    $(this).removeClass("darken-2");
-    $(".bio, .ideas").addClass("darken-2");
-});
-$(".ideas").click(function() {
-    $(".about .t4").show();
-    $(".about .t3, .about .t2").hide();
-    $(this).removeClass("darken-2");
-    $(".bio, .interests").addClass("darken-2");
-});
-
-$(".projects").click(function() {
-    $(".proj .t2").show();
-    $(".proj .t3, .proj .t4").hide();
-    $(this).removeClass("darken-2");
-    $(".respon, .workexp").addClass("darken-2");
-    $.scrollify.update();
-});
-$(".respon").click(function() {
-    $(".proj .t3").show();
-    $(".proj .t2, .proj .t4").hide();
-    $(this).removeClass("darken-2");
-    $(".projects, .ideas").addClass("darken-2");
-    $.scrollify.update();
-});
-$(".workexp").click(function() {
-    $(".proj .t4").show();
-    $(".proj .t3, .proj .t2").hide();
-    $(this).removeClass("darken-2");
-    $(".projects, .respon").addClass("darken-2");
-    $.scrollify.update();
-});
-
-$(document).ready(function() {
-    $(".about .t3, .about .t4, .proj .t3, .proj .t4").hide();
-    $(".bio, .projects").toggleClass("darken-2");
-});
-
-$(function() {
-    $.scrollify({
-        section: ".diapo",
-        scrollbars: true,
-        overflowScroll: true,
-        touchScroll: true,
-        after: function(i, panels) {
-            anim(i, panels)
-        }
-    });
-
-    $(".pagination a").on("click", $.scrollify.move);
-});
