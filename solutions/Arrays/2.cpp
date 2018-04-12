483619e47e6494653d535b6c7f5280e030505344
Saksham Saxena <saksham_saxena@outlook.com>
2018-04-12 21:35:37 +0530

    sub array sum

--- solutions/Arrays/2.cpp
+++ solutions/Arrays/2.cpp
@@ -11,16 +11,16 @@ int main() {
 	int T;
 	cin >> T;
 	while(T--) {
-	    int N;
-	    cin >> N;
-	    int idealSum = N*(N+1)/2;
-	    int actualSum = 0;
-	    for(int i=0;i<N-1;i++) {
-	        int temp;
-	        cin >> temp;
-	        actualSum+=temp;
-	    }
-	    cout << (idealSum - actualSum) << endl;
+		int N;
+		cin >> N;
+		int idealSum = N*(N+1)/2;
+		int actualSum = 0;
+		for(int i=0;i<N-1;i++) {
+			int temp;
+			cin >> temp;
+			actualSum+=temp;
+		}
+		cout << (idealSum - actualSum) << endl;
 	}
 	return 0;
 }
