37cd18aa1c6e25cf8a0bc53780fe95b37666ad49
sakshambcs <saksham.saxena@bcstechnology.com.au>
2019-01-14 20:09:10 +0530

    Updated knapsack

--- solutions/DynamicProgramming/3.cpp
+++ solutions/DynamicProgramming/3.cpp
@@ -3,8 +3,8 @@
 	http://www.es.ele.tue.nl/education/5MC10/Solutions/knapsack.pdf
 */
 #include<iostream>
-using namespace std;
 
+using namespace std;
 int main() {
 	int T;
 	cin >> T;
@@ -18,25 +18,34 @@ int main() {
 		for(int i = 0; i < N; i++) {
 			cin >> wt[i];
 		}
-		int K[N+1][W+1];
-		for(int w = 1; w <= W; w++) {
+		int K[N+1][W+1], Keep[N+1][W+1];
+		for(int w = 0; w <= W; w++) {
 			K[0][w] = 0;
+			Keep[0][w] = 0;
 		}
 		for(int i = 0; i <= N; i++) {
 			K[i][0] = 0;
+			Keep[i][0] = 0;
 		}
 		for(int i = 1; i <= N; i++) {
 			for(int w = 1; w <= W; w++) {
-				if(w - wt[i-1] >= 0) {
-					int a = val[i-1] + K[i-1][w - wt[i-1]];
-					int b = K[i-1][w];
-					K[i][w] = max(a,b);
+				if(w - wt[i-1] >= 0 && val[i-1] + K[i-1][w - wt[i-1]] > K[i-1][w]) {
+					K[i][w] = val[i-1] + K[i-1][w - wt[i-1]];
+					Keep[i][w] = 1;
 				} else {
 					K[i][w] = K[i-1][w];
+					Keep[i][w] = 0;
 				}
 			}
 		}
 		cout << K[N][W] << endl;
+		int P = W;
+		for(int i = N; i >= 1; i--) {
+			if(Keep[i][P] == 1) {
+				cout << "Set {" << val[i-1] << ", " << wt[i-1] << "} was kept" << endl;
+				P = P - wt[i-1];
+			}
+		}
 	}
 	return 0;
 }
