dca1f81f40cc105e10abcb339215c380127d3bb7
sakshamsaxena <saksham_saxena@outlook.com>
2020-01-18 21:24:28 +0530

    Starting up

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Terminal extends React.Component {
+}
+
+class Buffer extends React.Component {
+}
+
+class Prompt extends React.Component {
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Terminal />,
+  document.getElementById('root')
+);
+
