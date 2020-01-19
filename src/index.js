39ef4b9f0c36de6765590bf458a3be019fe185d6
sakshamsaxena <saksham_saxena@outlook.com>
2020-01-19 13:41:24 +0530

    basic io done

--- src/index.js
+++ src/index.js
@@ -3,12 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Terminal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      buffers: []
+    };
+  }
+  checkPrompt(e) {
+    if (e.keyCode === 13 || e.charCode === 13) {
+      let t = e.target.value;
+      let u = this.state.buffers.length ? this.state.buffers.concat(t) : t;
+      this.setState({buffers: u});
+      return false;
+    }
+    return true;
+  }
+  render() {
+    return (
+      <div className="core-terminal">
+        <Buffer value={this.state.buffers}></Buffer>
+        <Prompt onKeyPress={(e)=> this.checkPrompt(e)} ></Prompt>
+      </div>
+    );
+  }
 }
 
 class Buffer extends React.Component {
+  render() {
+    return (
+      <div className="core-buffer">
+        {this.props.value}
+      </div>
+    );
+  }
 }
 
 class Prompt extends React.Component {
+  render() {
+    return (
+      <input type="text" className="core-prompt" onKeyPress={(e)=> this.props.onKeyPress(e)}></input>
+    );
+  }
 }
 
 // ========================================
@@ -17,4 +52,3 @@ ReactDOM.render(
   <Terminal />,
   document.getElementById('root')
 );
-
