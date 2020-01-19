ea5707990332bb4c5890baabd95dd0df9bafca46
sakshamsaxena <saksham_saxena@outlook.com>
2020-01-19 17:08:48 +0530

    fixed behaviour

--- src/index.js
+++ src/index.js
@@ -12,8 +12,13 @@ class Terminal extends React.Component {
   checkPrompt(e) {
     if (e.keyCode === 13 || e.charCode === 13) {
       let t = e.target.value;
-      let u = this.state.buffers.length ? this.state.buffers.concat(t) : t;
-      this.setState({buffers: u});
+      let buffer = {
+        command: t,
+        output: 'hello'
+      }
+      let u = this.state.buffers.length ? this.state.buffers.concat([buffer]) : [buffer];
+      this.setState({ buffers: u });
+      document.getElementsByClassName('core-prompt')[0].value = '';
       return false;
     }
     return true;
@@ -22,7 +27,7 @@ class Terminal extends React.Component {
     return (
       <div className="core-terminal">
         <Buffer value={this.state.buffers}></Buffer>
-        <Prompt onKeyPress={(e)=> this.checkPrompt(e)} ></Prompt>
+        <Prompt onKeyPress={(e) => this.checkPrompt(e)}></Prompt>
       </div>
     );
   }
@@ -30,8 +35,33 @@ class Terminal extends React.Component {
 
 class Buffer extends React.Component {
   render() {
+    let vals = this.props.value;
+    let buffers = vals.map((p) => {
+      return (
+        <div className="single-buffer">
+          <Command value={p.command}></Command>
+          <Output value={p.output}></Output>
+        </div>
+      );
+    });
+    return buffers;
+  }
+}
+
+class Output extends React.Component {
+  render() {
     return (
-      <div className="core-buffer">
+      <div className="buffer-output">
+        {this.props.value}
+      </div>
+    );
+  }
+}
+
+class Command extends React.Component {
+  render() {
+    return (
+      <div className="buffer-command">
         {this.props.value}
       </div>
     );
@@ -41,7 +71,7 @@ class Buffer extends React.Component {
 class Prompt extends React.Component {
   render() {
     return (
-      <input type="text" className="core-prompt" onKeyPress={(e)=> this.props.onKeyPress(e)}></input>
+      <input type="text" className="core-prompt" onKeyPress={(e) => this.props.onKeyPress(e)}></input>
     );
   }
 }
