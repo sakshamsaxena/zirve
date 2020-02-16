c131e6382ae9d9cd371cf4b0803d9e8c8ab6a829
sakshamsaxena <saksham_saxena@outlook.com>
2020-02-16 21:04:52 +0530

    added boilerplate for other commands

--- src/types/commands/ls.js
+++ src/types/commands/ls.js
@@ -1,5 +1,17 @@
 class Ls {
-  static args = [];
+  constructor() {
+    this.arguments = []
+  }
+  validateArgs(a) {
+    this.arguments = a;
+    return a.length === 0;
+  }
+  getInput() {
+    return "$ ls";
+  }
+  getOutput() {
+    return "listing of stuff";
+  }
 }
 
 export default Ls;
--- src/types/commands/tree.js
+++ src/types/commands/tree.js
@@ -1,5 +1,17 @@
 class Tree {
-  static args = [];
+  constructor() {
+    this.arguments = []
+  }
+  validateArgs(a) {
+    this.arguments = a;
+    return a.length === 0;
+  }
+  getInput() {
+    return "$ tree";
+  }
+  getOutput() {
+    return "tree view";
+  }
 }
 
 export default Tree;
